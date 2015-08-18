6273037541a68c103dc7930f2e3784f496dfcadd
Arjun Shrinivas <vmsnivas@gmail.com>
2015-08-18 12:26:15 +0530

    Create 003. Declaring of variables.sh

---
+++ 003. Declaring of variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author : Arjun Shrinivas
+# Date : 18th Aug 2015
+ 
+# Variables can be declared here
+declare -i A=20 C=5
+declare -i B=30
+declare -i SUM
+ 
+SUM=$A+$B+$C
+echo "The sum of $A, $B and $C is $SUM"
+ 
+# END
