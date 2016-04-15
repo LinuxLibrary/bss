88be31f9ad56325d490249e2cb03aea1a9063e36
Arjun Shrinivas <vmsnivas@gmail.com>
2016-04-15 10:26:28 +0000

    Added 26.File_Mod_Hist_Check.sh

---
+++ scripts/26.File_Mod_Hist_Check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author : Arjun Shrinivas
+# Purpose : To know the modification history of a specific file
+
+echo -e "Usage : $0 <filename>"
+FILE=$1
+ 
+USERS=`grep /bin/bash /etc/passwd | grep -v root | awk -F: '{print $1}'`
+USERNAME=`echo $USERS`
+ 
+if [[ ! -z results.txt ]]
+then
+        :> results.txt
+fi
+for i in `echo $USERS`
+ do
+        HISTORY="/home/$i/.bash_history"
+	ENCDATE=`sed -ne '/'"$FILE"'/{x;p;d;}' -e x $HISTORY | grep -v ^[a-z] | tail -n1`
+        if [[ ! -z $ENCDATE ]]
+        then
+		COMMAND=`sed -ne '/'"$ENCDATE"'/,+1p' $HISTORY | tail -n1`
+		MODDATE=`echo $ENCDATE | tr '#' '@'`
+		DATE=`date -d $MODDATE +'%F %T'`
+                echo -e "$i\t$DATE\t$COMMAND" >> results.txt
+        fi
+ done
+
+more results.txt | sort -k2
