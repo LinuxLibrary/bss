115a13639bdfd4a290e46c6191aad65d05fc47d2
Arjun Shrinivas <vmsnivas@gmail.com>
2016-04-28 11:58:24 +0000

    MSTR-BASH Modified 01.shifting.sh

--- practice/01.shifting.sh
+++ practice/01.shifting.sh
@@ -8,13 +8,9 @@ NUMBER=$#
 while [ $COUNT -lt $NUMBER ]
 do
 	COUNT=`expr $COUNT + 1`
-#	COUNT += 1
-	echo $COUNT
-	TOKEN=$`echo $COUNT`
-	echo $TOKEN
-	ARG=`echo $TOKEN`
-	echo $ARG
-	echo -e "\tArgument $COUNT: $# \n"
+	TOKEN='$'"$COUNT"
+	ARG=`echo $* | awk '{print $1}'`
+	echo -e "\tArgument $COUNT: $ARG \n"
 	shift
 done
 # END
