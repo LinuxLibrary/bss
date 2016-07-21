13b292fd7bf50c2537cad869c0b3b98958700c9a
Arjun Shrinivas <vmsnivas@gmail.com>
2016-07-21 08:57:07 +0530

    Added script to automate git commits for all repositories

---
+++ scripts/30.gitupdate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Please input your Repos parent directory"
+read REPODIR
+echo
+#REPODIR="/e/Practice/GitRepos"
+for i in `echo $(ls $REPODIR | tr -d '/')`
+do
+	echo "Changing to $REPODIR/$i"
+	cd $REPODIR/$i
+	GITURL=`git remote -v | grep fetch | awk '{print $2}'`
+	echo "Pulling all the changes from $GITURL ..."
+	git pull -ff > /dev/null
+	echo "Pushing all local commits to $GITURL ..."
+	git push -u origin master > /dev/null
+	echo
+done
