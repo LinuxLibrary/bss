3e2508b297dc6756ce75f6dfb7e8b3a0efcccf8d
Arjun Shrinivas <vmsnivas@gmail.com>
2016-08-25 20:32:46 +0000

    Modified JiraCount.sh

--- practice/JiraCount.sh
+++ practice/JiraCount.sh
@@ -3,8 +3,8 @@
 ENVS="production preprod stage test dev blade"
 for ENV in $ENVS
 do
-	URL="https://spscommerce.atlassian.net/rest/api/2/search?jql=project%20%3D%20CHGMGMT%20and%20cf%5B16002%5D%20%3D%20$ENV"
-	RESULT=$(curl -s -u displaymonitor:CDZha6u8 $URL)
+	URL="https://XXXXXXXXXXX.atlassian.net/rest/api/2/search?jql=project%20%3D%20CHGMGMT%20and%20cf%5B16002%5D%20%3D%20$ENV"
+	RESULT=$(curl -s -u XXXXXXXXXX:xxxxxxxx $URL)
 	COUNT=$(echo $RESULT | python -c 'import sys, json; print json.load(sys.stdin)["total"]')
 	echo "$ENV=$COUNT"
 done
