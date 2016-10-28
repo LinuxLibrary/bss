bb5c3176dc33f57427a4c45b562927cac567011b
Arjun Shrinivas <vmsnivas@gmail.com>
2016-10-28 05:14:07 +0000

    Modified

--- scripts/31.gitadd_new.sh
+++ scripts/31.gitadd_new.sh
@@ -12,8 +12,7 @@ D=`git s | grep '^D\|^ D' | awk '{print $2}'`
 function add {
 	for i in `echo $A`
 	do
-		echo "Adding $i to staging..."
-		MSG="Added $i"
+		MSG=$(echo "Added $i")
 		git add $i
 		git commit -m $MSG
 	done
@@ -22,8 +21,7 @@ function add {
 function stgnew {
 	for i in `echo $U`
 	do
-		echo "Adding $i to staging..."
-		MSG="Added $i"
+		MSG=$(echo "Added $i")
 		git add $i
 		git commit -m $MSG
 	done
@@ -32,8 +30,7 @@ function stgnew {
 function modify {
 	for i in `echo $M`
 	do
-		echo "Modifying $i and staging..."
-		MSG="Modified $i"
+		MSG=$(echo "Modified $i")
 		git add $i
 		git commit -m $MSG
 	done
@@ -44,8 +41,7 @@ function rename {
 	do
 		R1=`git s | grep $i | awk '{print $2}'`
 		R2=`git s | grep $i | awk '{print $4}'`
-		echo "Renaming/Moving $R1 to $R2 and staging..."
-		MSG="Renamed/Moved $R1 to $R2"
+		MSG=$(echo "Renamed/Moved $R1 to $R2")
 		git add $R2
 		git commit -m $MSG
 	done
@@ -54,8 +50,7 @@ function rename {
 function delete {
 	for i in `echo $D`
 	do
-		echo "Removing $i ..."
-		MSG="Removed $i"
+		MSG=$(echo "Removed $i")
 #		git add $i
 		git commit -m $MSG
 	done
