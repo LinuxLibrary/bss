40359740fc5827f7b5d438c96478333431389057
Arjun Shrinivas <vmsnivas@gmail.com>
2017-02-06 11:30:37 +0530

    Modified scripts/27.Tomcat.sh

--- scripts/27.Tomcat.sh
+++ scripts/27.Tomcat.sh
@@ -2,38 +2,63 @@
 # Author : Arjun Shrinivas
 # Purpose : This script is usefull for managing tomcat
 
-CATALINA_HOME=/u01/tomcat
+# CATALINA_HOME=/u01/tomcat
+CATALINA_HOME=/opt/tomcat8
 OPTION=$1
 
 case $OPTION in
 start)
-	echo "Starting tomcat"
-	$CATALINA_HOME/bin/startup.sh
+	$CATALINA_HOME/bin/startup.sh >> /dev/null
+	if [[ $? -eq '0' ]]; then
+		echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+	else
+		echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+	fi
 ;;
 stop)
-	echo "Stopping tomcat"
-	$CATALINA_HOME/bin/shutdown.sh
+	$CATALINA_HOME/bin/shutdown.sh >> /dev/null
+	if [[ $? -eq '0' ]]; then
+		echo -e "\e[97mStopping Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+	else
+		echo -e "\e[97mStopping Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+	fi
 ;;
 restart)
 	PID=`ps -ef | grep catalina | grep -vi 'grep\|out$' | awk '{print $2}'`
 	if [[ -z $PID ]]
 	then
-		echo "Starting tomcat"
-		$CATALINA_HOME/bin/startup.sh
+		$CATALINA_HOME/bin/startup.sh >> /dev/null
+		if [[ $? -eq '0' ]]; then
+			echo -e "\e[97mRestarting Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+		else
+			echo -e "\e[97mRestarting Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+		fi
 	else
-		echo "Stopping tomcat"
-		$CATALINA_HOME/bin/shutdown.sh
-		sleep 20
+		$CATALINA_HOME/bin/shutdown.sh >> /dev/null
+		if [[ $? -eq '0' ]]; then
+			echo -e "\e[97mStopping Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+		else
+			echo -e "\e[97mStopping Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+		fi
+		sleep 10
 		PID=`ps -ef | grep catalina | grep -vi 'grep\|out$' | awk '{print $2}'`
 		if [[ -z $PID ]]
 		then
-			echo "Starting tomcat"
-			$CATALINA_HOME/bin/startup.sh
+			$CATALINA_HOME/bin/startup.sh >> /dev/null
+			if [[ $? -eq '0' ]]; then
+				echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+			else
+				echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+			fi
 		else
 			echo -e "Unable to shutdown tomcat. Force killing tomcat PID : $PID"
 			kill -9 $PID
-			echo "Starting tomcat"
-			$CATALINA_HOME/bin/startup.sh
+			$CATALINA_HOME/bin/startup.sh >> /dev/null
+			if [[ $? -eq '0' ]]; then
+				echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[32mOK \e[97m]"
+			else
+				echo -e "\e[97mStarting Tomcat\t\t[ \e[1m\e[31mFAILED \e[97m]"
+			fi
 		fi
 	fi
 ;;
@@ -41,9 +66,9 @@ status)
 	PID=`ps -ef | grep catalina | grep -vi 'grep\|out$' | awk '{print $2}'`
 	if [[ ! -z $PID ]]
 	then
-		echo -e "Tomcat is running on pid : $PID"
+		echo -e "\e[92mTomcat is running on pid : $PID\e[39m"
 	else
-		echo -e "Tomcat is not running"
+		echo -e "\e[31mTomcat is not running\e[39m"
 	fi
 ;;
 *)
